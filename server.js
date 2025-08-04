196b968a2ae1c7d16a2e42b7ce0e79ef823e612c
febriansyahardian <93551349+febriansyahardian@users.noreply.github.com>
2025-08-05 05:59:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const path = require('path'); // <--- ini penting
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // untuk Vercel, pakai port dari environment
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+
+// Set path views secara eksplisit
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/form', (req, res) => {
+  res.render('form');
 });
 
 app.post('/', (req, res) => {
