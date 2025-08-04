f1c2cecc22619bc1ea7de3355e8106538ebe3aba
febriansyahardian <93551349+febriansyahardian@users.noreply.github.com>
2025-08-05 05:06:11 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/form', (req, res) => {
+  res.render('form');
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send(`<h2>Thanks, ${req.body.name}! Your message has been received.</h2>`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
